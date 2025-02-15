69a4d00372c81c4bdd1a2fe64ee5eef42eaf22eb
Jewel Thomas <jewelat50@gmail.com>
2025-02-15 13:09:10 +0530

    backend work start

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+import connectDB from "./config/mongodb.js";
+
+//app config
+
+const app = express();
+const port = process.env.PORT || 5000;
+connectDB()
+
+//middleware
+
+app.use(express.json());
+app.use(cors());
+
+//api endpoints
+
+app.get("/", (req, res) => {
+    res.send("hello");
+});
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
